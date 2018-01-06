47de70a96dac168e7da4683f6abad4accf6f7dc6
tjburns08 <burns.tyler@gmail.com>
2018-01-06 13:08:01 +0100

    Documented the "bz.gmcsf.final" and the "combiend" datasets.

--- R/data.R
+++ R/data.R
@@ -11,10 +11,32 @@
 #' Bodenmiller-Zunder GM-CSF post-SCONE final data, that's been quantile
 #' normalized and z scored
 #'
-#' The post-SCONE output from a normalized and z scored Bodenmiller-Zunder
-#' dataset pair of fcs files, one untreated and one treated with GM-CSF.
+#' The post-SCONE output from a per-marker quantile normalized and z scored
+#  Bodenmiller-Zunder dataset pair of fcs files, one untreated and one
+#' treated with GM-CSF.
 #'
 #' @format A tibble of 10,000 cells by 69 features. This includes all the
 #' original parameters, the KNN-generated comparisons, differential
 #' abundance ("fraction.cond.2), and two t-SNE coordinates.
 "bz.gmcsf.final.norm.scale"
+
+#' Bodenmiller-Zunder GM-CSF post-SCONE final data
+#'
+#' The post-SCONE output from Bodenmiller-Zunder dataset pair of fcs files,
+#' one untreated and one treated with GM-CSF.
+#'
+#' @format A tibble of 9,998 cells by 69 features. This includes all the
+#' original parameters, the KNN-generated comparisons, differential
+#' abundance ("fraction.cond.2), and two t-SNE coordinates.
+"bz.gmcsf.final"
+
+#' The Wanderlust dataset, combined basal and IL7 conditions
+#'
+#' A single patient pair of basal and IL7 treated cells from bone marrow
+#' gated for B cell precursors.
+#'
+#' @format A tibble of 10,000 cells by 51 features, including all the input
+#' markers, Wanderlust values, and the condition. The first 5000 rows
+#' are untreated cells and the last 5000 rows are IL7 treated.
+"combined"
+
