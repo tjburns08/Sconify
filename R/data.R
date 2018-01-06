1a0ee597b9f1d5762f292f1f028dc266db6ab81b
tjburns08 <burns.tyler@gmail.com>
2018-01-06 12:24:21 +0100

    Started the documentation of the R data

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Basal data
+#'
+#' The basal cells from a single patient in the Wanderlust dataset
+#' @format A tibble of 10,000 cells by 51 features. All markers in the
+#' dataset, along with pre-calculated Wanderlust value and \code{condition}
+#' which is a string that denotes that this is the "basal" condition for
+#' each row. Important when this is concatenated with additional conditions
+"basal.data"
