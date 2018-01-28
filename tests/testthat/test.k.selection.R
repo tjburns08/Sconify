b8bded6115ffcf73afe5e89a7ccb7354926cdfcb
tjburns08 <burns.tyler@gmail.com>
2018-01-28 22:16:42 +0100

    Added two unit tests to the k selection function.

--- tests/testthat/test.k.selection.R
+++ tests/testthat/test.k.selection.R
@@ -3,4 +3,14 @@
 # Purpose: unit testing
 context("Testing the functionality of the ideal k finder")
 
+k.titration <- c(5, 50, 500)
+test <- impute.testing(k.titration = k.titration, cells = wand.il7, input.markers = input.markers, test.markers = funct.markers)
 
+test_that("High level desired output from the impute testing function", {
+    expect_equal(length(test), length(k.titration))
+})
+
+test_that("Convex loss function expected", {
+    expect_true(test[1] > test[2])
+    expect_true(test[2] < test[3])
+})
