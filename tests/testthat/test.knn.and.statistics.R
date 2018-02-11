e4158c4ca6738399adc5a19c5270a75c5e2dcaff
tjburns08 <burns.tyler@gmail.com>
2018-02-11 12:38:10 +0100

    Changed all function names from function.name to FunctionName

--- tests/testthat/test.knn.and.statistics.R
+++ tests/testthat/test.knn.and.statistics.R
@@ -6,8 +6,8 @@ library(Sconify)
 context("Test the knn and statistics arm of the sconify package")
 
 k <- 50
-test.nn <- fnn(wand.combined, input.markers = input.markers, k = k)
-test.scone <- scone.values(nn.matrix = test.nn,
+test.nn <- Fnn(wand.combined, input.markers = input.markers, k = k)
+test.scone <- SconeValues(nn.matrix = test.nn,
                     cell.data = wand.combined,
                     scone.markers = funct.markers,
                     unstim = "basal")
@@ -24,29 +24,29 @@ test_that("fnn produces a matrix of n rows (number of cells) and k columns (
 })
 
 test_that("fnn does not work when there is no input markers", {
-    expect_error(fnn(wand.combined, input.markers = c("hi"), k = k))
-    expect_error(fnn(wand.combined, input.markers = c(), k = k))
-    expect_error(fnn(wand.combined, input.markers = c("CD3"), k = k))
+    expect_error(Fnn(wand.combined, input.markers = c("hi"), k = k))
+    expect_error(Fnn(wand.combined, input.markers = c(), k = k))
+    expect_error(Fnn(wand.combined, input.markers = c("CD3"), k = k))
 })
 
 test_that("fnn does not work with some k values", {
-    expect_error(fnn(wand.combined, input.markers = input.markers, k = 0))
-    expect_error(fnn(wand.combined, input.markers = input.markers, k = -3))
+    expect_error(Fnn(wand.combined, input.markers = input.markers, k = 0))
+    expect_error(Fnn(wand.combined, input.markers = input.markers, k = -3))
 })
 
 test_that("knn density estimation is produced", {
-    tmp <- get.knn.de(test.nn)
+    tmp <- GetKnnDe(test.nn)
     expect_equal(length(tmp), nrow(wand.combined))
     expect_true(all(tmp > 0))
 })
 
 test_that("knn density estimation requires the list of two from fnn output", {
-    expect_error(get.knn.de(test.nn[[1]]))
-    expect_error(get.knn.de(test.nn[[2]]))
+    expect_error(GetKnnDe(test.nn[[1]]))
+    expect_error(GetKnnDe(test.nn[[2]]))
 })
 
 test_that("knn list is created for each cell", {
-    tmp <- make.knn.list(wand.combined, test.nn)
+    tmp <- MakeKnnList(wand.combined, test.nn)
     expect_equal(length(tmp), nrow(wand.combined))
     expect_true(all(colnames(tmp[[1]]) == colnames(wand.combined)))
     expect_equal(nrow(tmp[[1]]), k)
@@ -57,7 +57,7 @@ test_that("Scone values outputs a tibble of statistical values", {
 })
 
 test_that("Scone values produces a proper knn density estimation", {
-    expect_true(all(test.scone$density == get.knn.de(test.nn)))
+    expect_true(all(test.scone$density == GetKnnDe(test.nn)))
 })
 
 test_that("Scone produces a proper readout of differential abundance", {
@@ -65,16 +65,16 @@ test_that("Scone produces a proper readout of differential abundance", {
 })
 
 test_that("Scone wont perform statistics unless a proper test name is used", {
-    expect_error(scone.values(nn.matrix = test.nn,
-                              scone.markers = funct.markers,
-                              unstim = "basal",
-                              stat.test = "tyler's test"))
+    expect_error(SconeValues(nn.matrix = test.nn,
+                             scone.markers = funct.markers,
+                             unstim = "basal",
+                             stat.test = "tyler's test"))
 })
 
 test_that("Scone wont perform statistics unless a proper basal name is used", {
-    expect_error(scone.values(nn.matrix = test.nn,
-                              scone.markers = funct.markers,
-                              unstim = "bas"))
+    expect_error(SconeValues(nn.matrix = test.nn,
+                             scone.markers = funct.markers,
+                             unstim = "bas"))
 })
 
 test_that("Scone produces FDR adjusted q-values", {
@@ -90,11 +90,11 @@ test_that("Scone produces fold q thresholded fold changes", {
 
 
 test_that("Scone does multiple donor stats only with donors", {
-    expect_error(scone.values(nn.matrix = test.nn,
-                              cell.data = wand.combined,
-                              scone.markers = funct.markers,
-                              unstim = "basal",
-                              multiple.donor.compare = TRUE))
+    expect_error(SconeValues(nn.matrix = test.nn,
+                             cell.data = wand.combined,
+                             scone.markers = funct.markers,
+                             unstim = "basal",
+                             multiple.donor.compare = TRUE))
 })
 
 test_that("Scone values output q values and fold changes", {
